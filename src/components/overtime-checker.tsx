5c8d66f69d0f5e68a0e2de4925eff09e69fef138
AzureAD\吉田＠マリエッタ <ryuusei.yoshi2411@gmail.com>
2023-05-19 17:07:25 +0900

    [Add]残業時間計算処理

---
+++ src/components/overtime-checker.tsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useMemo, useState } from "react";
+
+export const OverTimeChecker: React.VFC = () => {
+    const operationsList = [
+        { value: 1, label: '東京', hour: 10 },
+        { value: 2, label: '佐賀', hour: 9 },
+        { value: 3, label: '福岡', hour: 10 },
+    ];
+    // 稼働時間
+    const [startTime, setStartTime] = useState('10:00');
+    const [startHour, setStartHour] = useState(10);
+    const [startMin, setStartMin] = useState(0);
+    const [selectOperation, setSelectOperation] = useState(1);
+    const displayOperationData = useMemo(() => {
+        return operationsList.find((v) => v.value === selectOperation);
+    }, [selectOperation]);
+    const [overTime, setOverTime] = useState('');
+    // プルメニュー変更処理
+    const handlePullChange = useCallback((e: React.ChangeEvent<HTMLSelectElement>) => {
+        const changeValue = Number(e.target.value);
+        if (!changeValue) {
+            window.alert('選択された値が正しくありません。');
+            return
+        };
+        let startTime2 = '';
+        if (changeValue === 2) {
+            startTime2 = String('0' + operationsList[changeValue - 1].hour + ':00');
+        } else {
+            startTime2 = String(operationsList[changeValue - 1].hour + ':00');
+        }
+        setSelectOperation(changeValue);
+        setStartTime(startTime2);
+        setStartHour(operationsList[changeValue - 1].hour);
+        setStartMin(0);
+        console.log("startMin", startHour, startMin);
+    }, [selectOperation, startTime, startHour, startMin]);
+
+    // 退勤ボタン押下処理
+    const handleLeavingWork = useCallback(() => {
+        const data = new Date();
+        const hour = data.getHours();
+        const min = data.getMinutes();
+        let overTimeHour = hour - startHour - 9;
+        let overTimeMin = min - startMin;
+        if (overTimeMin < 0) {
+            overTimeMin += 60;
+            overTimeHour--;
+        }
+        if (overTimeHour > 0 &&
+            overTimeMin >= 0) {
+            setOverTime(String(overTimeHour + '時間' + overTimeMin + '分残業しました'));
+        } else if (overTimeHour === 0 &&
+            overTimeMin >= 0) {
+            setOverTime(String(overTimeMin + '分残業しました'));
+        } else {
+            window.alert('まだ定時じゃありません。');
+        }
+    }, [overTime, startHour, startMin]);
+
+    // 出勤時間変更処理
+    const handleTimeChange = useCallback((e: { target: { value: any; }; }) => {
+        const timeValue = e.target.value;
+        let [hour, min]: number[] = timeValue.split(':');
+        if (hour < 10) {
+            hour = (hour / 10) * 10;
+        }
+        if (min < 10) {
+            min = (min / 10) * 10;
+        }
+        setStartTime(timeValue);
+        setStartHour(hour);
+        setStartMin(min);
+    }, [startTime, startHour, startMin])
+    return (
+        <div style={{ display: 'flex', padding: '1rem', alignItems: 'center', gap: '4px', flexDirection: 'column' }}>
+            <h1>残業チェッカー</h1>
+            <section>
+                <select value={selectOperation} onChange={handlePullChange}>
+                    {operationsList.map((operation) => (
+                        <option key={`operation_select_${operation.value}`} value={operation.value}>
+                            {operation.label}
+                        </option>
+                    ))}
+                </select>
+            </section>
+            <input type="time" value={startTime} onChange={handleTimeChange}></input>
+            <button onClick={handleLeavingWork}>退勤</button>
+            <h4>残業時間：{overTime}</h4>
+        </div>
+    );
+}
